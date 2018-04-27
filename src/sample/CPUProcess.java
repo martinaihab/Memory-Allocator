0c549ff5a9f6abf650bc54f860143a95b2c89b65
martinaihab <martina.ihab96@gmail.com>
2018-04-27 04:43:06 +0200

    CPUProcess and Hole classes.

---
+++ src/sample/CPUProcess.java
@@ -0,0 +1,54 @@
+package sample;
+
+public class CPUProcess {
+    private int id;
+    private int size;
+    private String typeOfAlloc;
+    private int startAddress;
+
+    public CPUProcess(int id, int size, String typeOfAlloc) {
+        this.id = id;
+        this.size = size;
+        this.typeOfAlloc = typeOfAlloc;
+        this.startAddress = -1;
+    }
+
+    public CPUProcess(int id, int size, String typeOfAlloc, int startAddress) {
+        this.id = id;
+        this.size = size;
+        this.typeOfAlloc = typeOfAlloc;
+        this.startAddress = startAddress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getTypeOfAlloc() {
+        return typeOfAlloc;
+    }
+
+    public void setTypeOfAlloc(String typeOfAlloc) {
+        this.typeOfAlloc = typeOfAlloc;
+    }
+
+    public int getStartAddress() {
+        return startAddress;
+    }
+
+    public void setStartAddress(int startAddress) {
+        this.startAddress = startAddress;
+    }
+}
---
+++ src/sample/Hole.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class Hole {
+    private int size;
+    private int startAddress;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getStartAddress() {
+        return startAddress;
+    }
+
+    public void setStartAddress(int startAddress) {
+        this.startAddress = startAddress;
+    }
+}
